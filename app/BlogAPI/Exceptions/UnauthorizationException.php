2e8ef353635b1ae1542c2b55e5f9576e4d60a73b
liuliu <liuliugit@gmail.com>
2020-04-18 09:29:29 +0800

    [Add] http code 403 and 500 on exception.

---
+++ app/BlogAPI/Exceptions/UnauthorizationException.php
@@ -0,0 +1,28 @@
+<?php
+namespace BlogAPI\Exceptions;
+
+use Illuminate\Http\Response;
+
+class UnauthorizationException extends \Exception
+{
+    protected $code;
+    protected $message;
+    protected $previous;
+
+    public function __construct(string $message = "Unauthorization.", int $code = 403, \Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->code = $code;
+        $this->message = $message;
+        $this->previous = $previous;
+    }
+
+    public function render()
+    {
+        return response()->json([
+            'error_code' => $this->code,
+            'message' => $this->message,
+        ], Response::HTTP_FORBIDDEN);
+    }
+}
