d0cc685d2b5d45474c233cf325a53cf014b13fc9
liuliu <liuliugit@gmail.com>
2020-04-19 11:26:32 +0800

    [Add] darryldecode/cart for shopping cart feature.

--- config/app.php
+++ config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // https://github.com/darryldecode/laravelshoppingcart
+        Darryldecode\Cart\CartServiceProvider::class,
+
+
     ],
 
     /*
@@ -227,6 +231,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Cart' => Darryldecode\Cart\Facades\CartFacade::class,
     ],
 
 ];
