40210942d08e1d9f6af0c05f8a3adf25d62dd172
liuliu <liuliugit@gmail.com>
2020-04-19 17:24:56 +0800

    [Create] Orders table.

---
+++ database/migrations/2020_04_19_090745_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('order_no');
+            $table->string('trade_no');
+            $table->integer('total');
+            $table->string('paid_status');
+            $table->string('invoice_status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Orders');
+    }
+}
